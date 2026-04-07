ac7a73f06fea9365079c6d03d3eb6524a6c64ee5
Verchielxy <472865534@qq.com>
2026-04-07 16:34:00 +0800

    配置更新

---
+++ src/config/version.js
@@ -0,0 +1,24 @@
+const versionConfig = {
+	version: '1.1.0',
+	date: '2026-02-25',
+	history: [
+		{
+			version: '1.1.0',
+			date: '2026-02-25',
+			summary: '新增项目管理 项目列表 功能',
+			newFeature: [],
+			fixed: [],
+			patch: [],
+		},
+		{
+			version: '1.0.0',
+			date: '2026-02-25',
+			summary: '基础版本',
+			newFeature: [],
+			fixed: [],
+			patch: [],
+		},
+	],
+};
+
+export default versionConfig;
