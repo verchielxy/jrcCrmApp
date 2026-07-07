2a3e6e83121b506dae4422ec29766c69016fa210
Verchielxy <472865534@qq.com>
2026-07-07 11:33:35 +0800

    配置更新

--- src/config/business/engineering.js
+++ src/config/business/engineering.js
@@ -1,6 +1,38 @@
 
 const engineeringConfig = {
+	project: {
+		keys: ['engineering:all'],
+		canUserSet: (record) => {
+			return authentication.auths(engineeringConfig.project.keys);
+		},
+		// 是否可以进行物料申请
+		canMaterialApply: (status) => {
+			return status >= 11;
+		},
+		// 是否可以进行物料到场核验
+		canMaterialVerify: (status) => {
+			return status >= 11;
+		},
+		// 是否可以施工结束
+		canConstructionEnd: (status) => {
+			return status === 11;
+		},
+		// 是否可以施工验收
+		canConstructionAcceptance: (status) => {
+			return status >= 12;
+		},
+	},
 	materialApply : {
+		// 是否可以提交审核
+		canPush: (status) => {
+			return status === 0 || status === 2;
+		},
+		// 是否可以编辑和删除
+		canEdit: (status) => {
+			return status === 0 || status === 2;
+		},
+	},
+	spotPurchase : {
 		// 是否可以提交审核
 		canPush: (status) => {
 			return status === 0 || status === 2;
